c27b33f9e0e40a4dbe2e7a344ffa3d0de5ca42b4
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-04-24 22:28:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1054,6 +1054,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
                                          callback_data=f'setgs#spell_check#{settings["spell_check"]}#{str(grp_id)}')
                 ],
                 [
+                    InlineKeyboardButton('filter',
+                                         callback_data=f'setgs#auto_filter#{settings["auto_filter"]}#{str(grp_id)}'),
+                    InlineKeyboardButton('✅ Yes' if settings["auto_filter"] else '❌ No',
+                                         callback_data=f'setgs#auto_filter#{settings["auto_filter"]}#{str(grp_id)}')
+                ],
+                [
                     InlineKeyboardButton('Welcome', callback_data=f'setgs#welcome#{settings["welcome"]}#{str(grp_id)}'),
                     InlineKeyboardButton('✅ Yes' if settings["welcome"] else '❌ No',
                                          callback_data=f'setgs#welcome#{settings["welcome"]}#{str(grp_id)}')
